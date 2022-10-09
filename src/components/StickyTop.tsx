edab484d60138317a73606693f99cc4bfc4da20d
katsuragawa <andre.katsuragawa@gmail.com>
2022-10-09 14:56:56 -0300

    feat: logo go to home

--- src/components/StickyTop.tsx
+++ src/components/StickyTop.tsx
@@ -1,17 +1,30 @@
+import { useRouter } from "next/router";
+
 type StickyTopProps = {
   children: JSX.Element;
 };
-export const StickyTop = ({ children }: StickyTopProps) => (
-  <div className="fixed top-0 flex w-full flex-col bg-white">
-    <div className="flex h-16 items-center justify-between rounded-b-full bg-hack-blue-dark px-24">
-      <div className="text-2xl font-bold text-white">avalizo</div>
-      <div className="h-10 w-10 rounded-full bg-white"></div>
+
+export const StickyTop = ({ children }: StickyTopProps) => {
+  const router = useRouter();
+  const goHome = () => router.push("/");
+
+  return (
+    <div className="fixed top-0 flex w-full flex-col bg-white">
+      <div className="flex h-16 items-center justify-between rounded-b-full bg-hack-blue-dark px-24">
+        <div
+          className="cursor-pointer text-2xl font-bold text-white"
+          onClick={goHome}
+        >
+          avalizo
+        </div>
+        <div className="h-10 w-10 rounded-full bg-white"></div>
+      </div>
+      <div className="mx-auto mt-16 mb-8 flex w-[720px] flex-col justify-center">
+        {children}
+      </div>
+      <div className="mx-12 flex items-center justify-between">
+        <div className="h-1 w-full rounded-full bg-hack-blue-dark" />
+      </div>
     </div>
-    <div className="mx-auto mt-16 mb-8 flex w-[720px] flex-col justify-center">
-      {children}
-    </div>
-    <div className="mx-12 flex items-center justify-between">
-      <div className="h-1 w-full rounded-full bg-hack-blue-dark" />
-    </div>
-  </div>
-);
+  );
+};
